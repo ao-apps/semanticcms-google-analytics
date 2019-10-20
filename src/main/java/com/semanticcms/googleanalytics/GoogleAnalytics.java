149350292d7bb4e11ac7414bf5fa94d080823947
AO Industries, Inc. <support@aoindustries.com>
2019-10-20 06:45:25 -0500

    Now outputting HTML or XHTML based on the current response content type.
    This should eliminate validator warnings when used as HTML.

--- src/main/java/com/semanticcms/googleanalytics/GoogleAnalytics.java
+++ src/main/java/com/semanticcms/googleanalytics/GoogleAnalytics.java
@@ -22,10 +22,12 @@
  */
 package com.semanticcms.googleanalytics;
 
-import static com.aoindustries.encoding.JavaScriptInXhtmlEncoder.javaScriptInXhtmlEncoder;
 import com.aoindustries.encoding.MediaWriter;
 import static com.aoindustries.encoding.TextInJavaScriptEncoder.encodeTextInJavaScript;
-import static com.aoindustries.encoding.TextInXhtmlAttributeEncoder.textInXhtmlAttributeEncoder;
+import com.aoindustries.html.Doctype;
+import com.aoindustries.html.Html;
+import com.aoindustries.html.Link;
+import com.aoindustries.html.servlet.HtmlEE;
 import com.aoindustries.net.URIEncoder;
 import com.semanticcms.core.model.Page;
 import com.semanticcms.core.renderer.html.Component;
@@ -65,28 +67,38 @@ public class GoogleAnalytics implements Component {
 		Page page,
 		ComponentPosition position
 	) throws ServletException, IOException {
-		if(position == ComponentPosition.HEAD_START) {
-			// See https://rehmann.co/blog/optimize-google-analytics-google-tag-manager-via-preconnect-headers/
-			out.write("<link href=\"https://www.google-analytics.com\" rel=\"dns-prefetch\" />\n"
-				+ "<link href=\"https://www.google-analytics.com\" rel=\"preconnect\" crossorigin=\"anonymous\" />\n"
+		Html html = HtmlEE.get(servletContext, request, out);
+		if(html.doctype == Doctype.HTML5) {
+			if(position == ComponentPosition.HEAD_START) {
+				// See https://rehmann.co/blog/optimize-google-analytics-google-tag-manager-via-preconnect-headers/
+				html.link().href("https://www.google-analytics.com").rel(Link.Rel.DNS_PREFETCH).__().nl();
+				html.link().href("https://www.google-analytics.com").rel(Link.Rel.PRECONNECT).crossorigin(Link.Crossorigin.ANONYMOUS).__().nl();
 				// + "<!-- Global site tag (gtag.js) - Google Analytics -->\n"
-				+ "<script type=\"text/javascript\" async=\"async\" src=\"https://www.googletagmanager.com/gtag/js?id=");
-			URIEncoder.encodeURIComponent(
-				trackingId,
-				textInXhtmlAttributeEncoder,
-				out
-			);
-			out.write("\"></script>\n"
-				+ "<script type=\"text/javascript\">\n");
-			MediaWriter scriptOut = new MediaWriter(javaScriptInXhtmlEncoder, out);
-			scriptOut.write("  window.dataLayer = window.dataLayer || [];\n"
-				+ "  function gtag(){dataLayer.push(arguments);}\n"
-				+ "  gtag('js', new Date());\n"
-				// + "\n"
-				+ "  gtag('config', '");
-			encodeTextInJavaScript(trackingId, scriptOut);
-			scriptOut.write("');\n");
-			out.write("</script>");
+				// TODO: Attribute streaming src
+				html.script().async(true).src("https://www.googletagmanager.com/gtag/js?id=" + URIEncoder.encodeURIComponent(trackingId)).__().nl();
+				try (MediaWriter script = html.script().out()) {
+					script.write("  window.dataLayer = window.dataLayer || [];\n"
+						+ "  function gtag(){dataLayer.push(arguments);}\n"
+						+ "  gtag('js', new Date());\n"
+						// + "\n"
+						+ "  gtag('config', '");
+					encodeTextInJavaScript(trackingId, script);
+					script.write("');\n");
+				}
+			}
+		} else {
+			if(position == ComponentPosition.HEAD_END) {
+				try (MediaWriter script = html.script().out()) {
+					script.write("(function(i,s,o,g,r,a,m){i[\"GoogleAnalyticsObject\"]=r;i[r]=i[r]||function(){\n"
+						+ "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n"
+						+ "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n"
+						+ "})(window,document,\"script\",\"https://www.google-analytics.com/analytics.js\",\"ga\");\n"
+						+ "ga(\"create\",\"");
+					encodeTextInJavaScript(trackingId, script);
+					script.write("\",\"auto\");\n"
+						+ "ga(\"send\",\"pageview\");\n");
+				}
+			}
 		}
 	}
 }
