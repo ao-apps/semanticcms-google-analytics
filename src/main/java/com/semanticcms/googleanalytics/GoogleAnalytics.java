01a148471551d558816a3d3ee614d14edef9f9d8
AO Industries, Inc. <support@aoindustries.com>
2019-10-29 01:05:41 -0500

    Added Google Analytics writing utility.

--- src/main/java/com/semanticcms/googleanalytics/GoogleAnalytics.java
+++ src/main/java/com/semanticcms/googleanalytics/GoogleAnalytics.java
@@ -22,13 +22,9 @@
  */
 package com.semanticcms.googleanalytics;
 
-import com.aoindustries.encoding.MediaWriter;
-import static com.aoindustries.encoding.TextInJavaScriptEncoder.encodeTextInJavaScript;
 import com.aoindustries.html.Doctype;
 import com.aoindustries.html.Html;
-import com.aoindustries.html.Link;
 import com.aoindustries.html.servlet.HtmlEE;
-import com.aoindustries.net.URIEncoder;
 import com.semanticcms.core.model.Page;
 import com.semanticcms.core.servlet.Component;
 import com.semanticcms.core.servlet.ComponentPosition;
@@ -41,8 +37,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Adds the Google Analytics <a href="https://support.google.com/analytics/answer/1008080?hl=en&ref_topic=1008079#GA">Global Site Tag</a> just after head start.
+ * For {@link Doctype#HTML5}, adds the Google Analytics <a href="https://support.google.com/analytics/answer/1008080?hl=en&ref_topic=1008079#GA">Global Site Tag</a>
+ * at {@link ComponentPosition#HEAD_START}.
+ * For {@linkplain Doctype other doctypes}, adds a more traditional Google Analytics script
+ * at {@link ComponentPosition#HEAD_END}.
  * This is applied to all {@link View views} and all {@link Page pages}, even those that are "noindex".
+ *
+ * @see com.aoindustries.html.util.GoogleAnalytics#writeGlobalSiteTag(com.aoindustries.html.Html, java.lang.String)
+ * @see com.aoindustries.html.util.GoogleAnalytics#writeAnalyticsJs(com.aoindustries.html.Html, java.lang.String)
  */
 public class GoogleAnalytics implements Component {
 
@@ -70,34 +72,11 @@ public class GoogleAnalytics implements Component {
 		Html html = HtmlEE.get(servletContext, request, out);
 		if(html.doctype == Doctype.HTML5) {
 			if(position == ComponentPosition.HEAD_START) {
-				// See https://rehmann.co/blog/optimize-google-analytics-google-tag-manager-via-preconnect-headers/
-				html.link().href("https://www.google-analytics.com").rel(Link.Rel.DNS_PREFETCH).__().nl();
-				html.link().href("https://www.google-analytics.com").rel(Link.Rel.PRECONNECT).crossorigin(Link.Crossorigin.ANONYMOUS).__().nl();
-				// + "<!-- Global site tag (gtag.js) - Google Analytics -->\n"
-				// TODO: Attribute streaming src
-				html.script().async(true).src("https://www.googletagmanager.com/gtag/js?id=" + URIEncoder.encodeURIComponent(trackingId)).__().nl();
-				try (MediaWriter script = html.script().out()) {
-					script.write("  window.dataLayer = window.dataLayer || [];\n"
-						+ "  function gtag(){dataLayer.push(arguments);}\n"
-						+ "  gtag('js', new Date());\n"
-						// + "\n"
-						+ "  gtag('config', '");
-					encodeTextInJavaScript(trackingId, script);
-					script.write("');\n");
-				}
+				com.aoindustries.html.util.GoogleAnalytics.writeGlobalSiteTag(html, trackingId);
 			}
 		} else {
 			if(position == ComponentPosition.HEAD_END) {
-				try (MediaWriter script = html.script().out()) {
-					script.write("(function(i,s,o,g,r,a,m){i[\"GoogleAnalyticsObject\"]=r;i[r]=i[r]||function(){\n"
-						+ "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n"
-						+ "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n"
-						+ "})(window,document,\"script\",\"https://www.google-analytics.com/analytics.js\",\"ga\");\n"
-						+ "ga(\"create\",\"");
-					encodeTextInJavaScript(trackingId, script);
-					script.write("\",\"auto\");\n"
-						+ "ga(\"send\",\"pageview\");\n");
-				}
+				com.aoindustries.html.util.GoogleAnalytics.writeAnalyticsJs(html, trackingId);
 			}
 		}
 	}
