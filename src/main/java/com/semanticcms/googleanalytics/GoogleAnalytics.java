18ef56f1c93f6fb5e3e58ec0a3a97ab5ec6dc1a0
AO Industries, Inc <support@aoindustries.com>
2021-03-15 04:13:49 -0500

    New project "ao-fluent-html-any"

--- src/main/java/com/semanticcms/googleanalytics/GoogleAnalytics.java
+++ src/main/java/com/semanticcms/googleanalytics/GoogleAnalytics.java
@@ -45,8 +45,8 @@ import javax.servlet.http.HttpServletResponse;
  * Both are added at {@link ComponentPosition#HEAD_START}.
  * This is applied to all {@link View views} and all {@link Page pages}, even those that are "noindex".
  *
- * @see com.aoindustries.html.util.GoogleAnalytics#writeGlobalSiteTag(com.aoindustries.html.AnyDocument, java.lang.String)
- * @see com.aoindustries.html.util.GoogleAnalytics#writeAnalyticsJs(com.aoindustries.html.AnyDocument, java.lang.String)
+ * @see com.aoindustries.html.util.GoogleAnalytics#writeGlobalSiteTag(com.aoindustries.html.any.AnyDocument, java.lang.String)
+ * @see com.aoindustries.html.util.GoogleAnalytics#writeAnalyticsJs(com.aoindustries.html.any.AnyDocument, java.lang.String)
  */
 public class GoogleAnalytics implements Component {
 
