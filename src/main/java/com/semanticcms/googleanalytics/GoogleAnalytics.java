3caba4a8906d9337fda89d45f1d8468ae7b3241e
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:48 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/googleanalytics/GoogleAnalytics.java
+++ src/main/java/com/semanticcms/googleanalytics/GoogleAnalytics.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-google-analytics.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.googleanalytics;
 
 import com.aoapps.encoding.Doctype;
